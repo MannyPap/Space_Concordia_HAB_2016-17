51e38bffbf0f626b1f4723da0f72452d09742f8f
MannyPap <pap-man@hotmail.com>
2017-01-16 16:34:58 -0500

    Files related to ACS and calibrated imu sensor.

---
+++ Razor_AHRS/setup_loop.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+
+Servo esc;
+
+void setup()
+{
+  setup_ACS();
+  esc.attach(9); //pin used for esc signal on arduino
+  esc.writeMicroseconds(1060); //arm the motor to zero speed, failing to do this will crank the motor to 100% on startup
+  delay(1000);
+  }
+
+void loop()
+{
+  loop_ACS();
+  int val;
+  val = TO_DEG(yaw);
+  val = map(val,-180,+180, 1060,1860); //1860 is max
+  esc.writeMicroseconds(val);
+}
+
